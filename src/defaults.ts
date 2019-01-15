8840d36af47a2be0499a8cd23676ac19ac26fe18
Paul Bates <paul.a.bates@gmail.com>
2019-01-15 00:16:35 -0800

    patch: Increase discovery timeout to 1 minute

--- src/defaults.ts
+++ src/defaults.ts
@@ -1,5 +1,5 @@
-// Default discovery timeout 30 seconds
-export const DEVICE_DISCOVERY_TIMEOUT_MS = 30 * 1000
+// Default discovery timeout 60 seconds
+export const DEVICE_DISCOVERY_TIMEOUT_MS = 60 * 1000
 
 // Default connection timeout 10 seconds
 export const DEVICE_CONNECT_TIMEOUT_MS = 10 * 1000
