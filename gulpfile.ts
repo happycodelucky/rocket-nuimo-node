cd79aaf32218baa81d32edf320d5e688282609b6
Paul Bates <paul.a.bates@gmail.com>
2019-01-17 23:03:07 -0800

    build: Added tasks to modify package.json in preparation

--- gulpfile.ts
+++ gulpfile.ts
@@ -13,6 +13,7 @@
 import * as del from 'del'
 import * as typescript from 'typescript'
 import * as merge from 'merge2'
+import * as jsonEditor from 'gulp-json-editor'
 
 import gulpTsLint from 'gulp-tslint'
 
@@ -24,10 +25,10 @@ import { createProject as createTsProject } from 'gulp-typescript'
 import { dest, parallel, series, src } from 'gulp'
 
 // Build destination paths
-const ARTIFACTS_DEST = 'build'  // Build artificats
-const COVERAGE_DEST = 'build'   // Coverage reports
-const LIB_DEST = 'lib'          // Javascript sources
-const TYPEDEF_DEST = 'dts'      // Defintion files
+const ARTIFACTS_DEST = 'publish'    // Build artificats
+const COVERAGE_DEST = 'coverage'    // Coverage reports
+const LIB_DEST = 'lib'              // Javascript sources
+const TYPEDEF_DEST = 'dts'          // Defintion files
 
 // Package projects
 const PROJECT_TSCONFIG = './src/tsconfig.json'
@@ -53,10 +54,13 @@ export function clean() {
 }
 
 /**
- * Performs a lint on package sources
+ * Performs a lint on package & example sources
  */
-export function lint() {
-    return lintSources(tsProject())
+export function lint(done: TaskCallback) {
+    return parallel(
+        lintSources.bind(undefined, tsProject()),
+        lintSources.bind(undefined, tsExamplesProject()),
+    )(done)
 }
 
 /**
@@ -67,28 +71,28 @@ export function build() {
 }
 
 /**
- * Cleans packaged artifacts
- */
-export function cleanArtifacts() {
-    return del(ARTIFACTS_DEST)
-}
-
-/**
  * Packages up the package and assembles an set of artifacts for publishing
  */
-export function packageArtifacts(done: TaskCallback) {
+export function preparePublish(done: TaskCallback) {
     const project = tsProject()
     const artifactsPath = ARTIFACTS_DEST
 
     return series(
-        cleanArtifacts,
+        () => del(ARTIFACTS_DEST),
         lintSources.bind(undefined, project),
         transpileSources.bind(undefined, project, artifactsPath),
         parallel(
             copyPackageFiles.bind(undefined, artifactsPath),
             copyTypeScriptProjectSources.bind(undefined, project, `${artifactsPath}/src`),
         ),
+        updatePackageJson.bind(undefined, artifactsPath),
     )(done)
+}
+
+/**
+ *
+ */
+export function publish(done: TaskCallback) {
 }
 
 //
@@ -98,7 +102,7 @@ export function packageArtifacts(done: TaskCallback) {
 /**
  * Package default project
  */
-export function tsProject(): Project {
+function tsProject(): Project {
     return createTsProject(PROJECT_TSCONFIG, {
         typescript,
         declaration: true,
@@ -157,4 +161,21 @@ function copyTypeScriptProjectSources(project: Project, destPath: string) {
  */
 function copyPackageFiles(artifactsPath: string) {
     return src(PACKAGE_FILES_GLOB).pipe(dest(artifactsPath))
+}
+
+/**
+ * Updates the package.json configuration
+ */
+function updatePackageJson(artifactsPath: string) {
+    return src(`${artifactsPath}/package.json`)
+        .pipe(jsonEditor((json: Record<string, any>) => {
+            json.main = `${LIB_DEST}/index.js`
+            json.types = `${TYPEDEF_DEST}/index.d.ts`
+            json.devDependencies = undefined
+            json.private = undefined
+            json.scripts = undefined
+
+            return json
+        }))
+        .pipe(dest(artifactsPath))
 }
